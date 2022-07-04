337220227c5da9e7e9223074609876af8e181f46
SanyaSukhariev <sukhariev0212@gmail.com>
2022-07-04 22:43:19 +0300

    Update spreadsheet.js
    working with dom

--- spreadsheet.js
+++ spreadsheet.js
@@ -27,4 +27,28 @@ row_1.appendChild(head_2);
 row_1.appendChild(head_3);
 thead.appendChild(row_1);
 
-for (let i = 1; i <= 3; i++) {}
+let row_2 = document.createElement("tr");
+let data_1 = document.createElement("td");
+data_1.innerHTML = "1.";
+let data_2 = document.createElement("td");
+data_2.innerHTML = "James Clerk";
+let data_3 = document.createElement("td");
+data_3.innerHTML = "Netflix";
+
+row_2.appendChild(data_1);
+row_2.appendChild(data_2);
+row_2.appendChild(data_3);
+tbody.appendChild(row_2);
+
+let row_3 = document.createElement("tr");
+let row_3_data_1 = document.createElement("td");
+row_3_data_1.innerHTML = "2.";
+let row_3_data_2 = document.createElement("td");
+row_3_data_2.innerHTML = "Adam White";
+let row_3_data_3 = document.createElement("td");
+row_3_data_3.innerHTML = "Microsoft";
+
+row_3.appendChild(row_3_data_1);
+row_3.appendChild(row_3_data_2);
+row_3.appendChild(row_3_data_3);
+tbody.appendChild(row_3);
